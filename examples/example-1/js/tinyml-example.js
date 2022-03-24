b7717846884ba3ceef23c8b7cac40593c05db601
Iuriiiii <metralicnetwork@gmail.com>
2022-03-24 01:44:46 -0300

    example fix

---
+++ examples/example-1/js/tinyml-example.js
@@ -0,0 +1,38 @@
+﻿function body()
+{
+    return document.documentElement;
+}
+
+/* https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file */
+function readfile(file)
+{
+    let rawFile = new XMLHttpRequest();
+    
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                
+                let tml = new TinyML(allText);
+                
+                if(tml.parse())
+                    document.documentElement.innerHTML = tml.html_source;
+                else
+                    document.documentElement.innerHTML = tml.description() + "<br>" + tml.code();
+                
+                body().innerHTML = tml.html_source;
+            }
+        }
+    }
+    
+    rawFile.send(null);
+}
+
+window.onload = function()
+{
+    readfile("main.tml");
+}
